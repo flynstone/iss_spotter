80755c93cfa34e9c34131264fb7826c1b31bdacd
Julien <flynstone@live.ca>
2021-12-03 04:01:34 +0000

    commit final project

---
+++ index.js
@@ -0,0 +1,73 @@
+const { nextISSTimesForMyLocation } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+// 
+//   console.log('It worked! Returned IP:', ip);
+// });
+
+
+// fetchCoordsByIP("xxx.xx.xx.xx", (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work", error);
+//     return;
+//   }
+//   console.log("It worked! Returned Coords:", coordinates);
+// });
+
+
+// const exampleCoords = { latitude: '45.609900', longitude: '-74.591480' };
+// 
+// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+// 
+//   console.log('It worked! Returned flyover times:' , passTimes);
+// });
+
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results. 
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */
+
+/** 
+ * Input: 
+ *   Array of data objects defining the next fly-overs of the ISS.
+ *   [ { risetime: <number>, duration: <number> }, ... ]
+ * Returns: 
+ *   undefined
+ * Sideffect: 
+ *   Console log messages to make that data more human readable.
+ *   Example output:
+ *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
+ */
+
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+
+
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
